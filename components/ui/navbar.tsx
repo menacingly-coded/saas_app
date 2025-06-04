9ca535ecb8544d4f5b7202eff365662abd108f59
Shreya Sharma <shreya0987.edu@gmail.com>
2025-06-04 12:54:04 +0530

    added Navbar and NavItems

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
+import Navitems from "./Navitems";
+
 const Navbar = () => {
     return (
         <nav className="navbar">
@@ -12,9 +14,7 @@ const Navbar = () => {
                 </div>
             </Link>
             <div className="flex items-center gap-8">
-                <p>Home</p>
-                <p>Companions</p>
-                <p>My journey</p>
+                <Navitems/>
                 <p>Sign In</p>
             </div>
         </nav>
