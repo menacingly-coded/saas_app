99c7536c6e0e17e660005a2b26a53d5d0b10f294
Shreya Sharma <shreya0987.edu@gmail.com>
2025-06-14 16:56:51 +0530

    updated companionCard

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,31 @@ const Page = () => {
   return (
     <main>
       <h1>Popular Companions</h1>
-      <section>
-        <CompanionCard />
-        <CompanionCard />
-        <CompanionCard />
+      <section className="home-section">
+        <CompanionCard 
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives and Integrals"
+          subject="maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard 
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="language"
+          subject="English Literature"
+          duration={30}
+          color="#BDE7FF"
+        />
       </section>
 
       <section className="home-section">
