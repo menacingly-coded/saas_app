6c21a0c5e28dc9faa6bf7e856c27af509c8fc7d4
Shreya Sharma <shreya0987.edu@gmail.com>
2025-07-04 03:40:02 +0530

    integrated supabse in sign-in

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import {SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Navitems from "./Navitems";
 
 const Navbar = () => {
@@ -9,13 +10,22 @@ const Navbar = () => {
                 <div className="flex items-center gap-2.5 cursor-pointer">
                     <Image 
                         src="/images/logo.svg" 
-                        alt="logo" width ={46} height= {44}
+                        alt="logo" 
+                        width ={46} 
+                        height= {44}
                     />
                 </div>
             </Link>
             <div className="flex items-center gap-8">
                 <Navitems/>
-                <p>Sign In</p>
+                <SignedOut>
+                    <SignInButton>
+                        <button className="btn-=signin">Sign In</button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
             </div>
         </nav>
     )
