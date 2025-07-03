d9eae95e278b1565b81aa4110cc4bb6f803471cb
Shreya Sharma <shreya0987.edu@gmail.com>
2025-07-03 20:22:37 +0530

    added icons

--- components/CompanionsList.tsx
+++ components/CompanionsList.tsx
@@ -10,6 +10,7 @@ import {
 
 import { cn } from "@/lib/utils"; // or wherever your `cn` utility is defined
 import Link from "next/link";
+import Image from 'next/image';
 
 
 interface CompanionsListProps{
@@ -32,22 +33,24 @@ const CompanionsList = ({title, companions, classNames}:CompanionsListProps ) =>
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    {companions?.map((companion)=>(
-                        <TableRow>
+                    {companions?.map(({id, subject, name, topic, duration})=>(
+                        <TableRow key={id}>
                             <TableCell>
-                                <Link href={`/companions/${companion.id}`}></Link>
+                                <Link href={`/companions/${id}`}>
+                                    <div className="flex items-center gap-2">
+                                        <div>
+                                            <Image 
+                                                src={`/icons/${subject}.svg`} 
+                                                alt={subject} 
+                                                width={35} 
+                                                height={35}/>
+                                        </div>
+                                    </div>
+                                </Link>
                             </TableCell>
                         </TableRow>
                     ))}
 
-                    
-                    
-                    <TableRow>
-                    <TableCell className="font-medium">INV001</TableCell>
-                    <TableCell>Paid</TableCell>
-                    <TableCell>Credit Card</TableCell>
-                    <TableCell className="text-right">$250.00</TableCell>
-                    </TableRow>
                 </TableBody>
             </Table>
         </article>
